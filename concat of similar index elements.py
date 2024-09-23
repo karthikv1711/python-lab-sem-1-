3c79a241f2089ccc5459c560ac5451bbace13b48
murali karthik vunnam <164615028+karthikv1711@users.noreply.github.com>
2024-09-23 16:06:18 +0530

    Create concat of similar index elements.py
    concat of similar index elements of given lists

---
+++ concat of similar index elements.py
@@ -0,0 +1,11 @@
+# concatenation of two lists
+
+l1 = ["M","na","i","Abhi"]
+l2 = ["y","me","s","Ram"]
+
+
+for i in l1 :
+	for j in l2 :
+		if l1.index(i) == l2.index(j) :
+			print(i+j,end=" ")
+print()
