05dec008fbdfc92cbf3278dbe8effe588a898b23
murali karthik vunnam <164615028+karthikv1711@users.noreply.github.com>
2024-10-14 16:03:19 +0530

    Create strlen.py
    4. Write a python program to find the length of the string without using the len() method

---
+++ strlen.py
@@ -0,0 +1,13 @@
+#4. Write a python program to find the length of the string without using the len() method?
+
+str = input('Enter a String : ')
+count = 0
+for i in str :
+    count = count+1
+print('String length : ',count)
+
+'''
+Enter a String : hello
+String length :  5
+
+'''
