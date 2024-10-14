17afcd31381f94fbd349dcc5ee7f3a9a21000f95
murali karthik vunnam <164615028+karthikv1711@users.noreply.github.com>
2024-10-14 16:19:21 +0530

    Create streven.py
    6. Write a Python program to print even length

---
+++ streven.py
@@ -0,0 +1,15 @@
+# 6. Write a Python program to print even length
+
+str = input('Enter a String : ').split()
+for i in str:
+    if len(i)%2 == 0 :
+        print(i)
+
+
+
+'''
+output:
+Enter a String : I am Human Being
+am
+
+'''
