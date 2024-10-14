7787cccf84d568a6bf46d16e876d0618bc894037
murali karthik vunnam <164615028+karthikv1711@users.noreply.github.com>
2024-10-14 15:55:24 +0530

    Create reversestring.py
    1. Write a python program to reverse a given string

---
+++ reversestring.py
@@ -0,0 +1,10 @@
+#1. Write a python program to reverse a given string?
+str = input('Enter a String : ')
+print('Reversed string :',str[::-1])
+
+''' 
+output:
+Enter a String : hello
+Reversed string : olleh
+
+'''
